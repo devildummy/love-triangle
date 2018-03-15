6eb3155ac7633b2b180b9ea1a75ff42f9d5b7279
devildummy <cyrok.13@gmail.com>
2018-03-15 18:34:05 +0300

    Function to get love trangles in index.html

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for( var i = 0; i < preferences.length; i++){
+          var first = preferences[i];
+          var second = preferences[preferences[i]-1];
+          var third = preferences[preferences[preferences[i]-1]-1];
+    if(first != second
+    && second != third
+    && third == (i+1))
+    {
+      count ++;
+    }
+  }
+  return(count/3);
 };
