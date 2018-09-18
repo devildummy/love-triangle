457da598bd07284e13f4bccd05c1005414e54017
devildummy <cyrok.13@gmail.com>
2018-09-19 00:26:54 +0300

    just new version, from blank space

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  var count = 0;
-  for( var i = 0; i < preferences.length; i++){
-          var first = preferences[i];
-          var second = preferences[preferences[i]-1];
-          var third = preferences[preferences[preferences[i]-1]-1];
-    if(first != second
-    && second != third
-    && third == (i+1))
-    {
-      count ++;
+  let count = 0;
+    let p = preferences;
+    let first, second, third;
+    for (let i=0;i<p.length;i++){
+        if(p[i]==undefined)
+        continue;
+      first = p[i];
+      second = p[first-1];
+      third = p[second-1];
+        
+      if (first!=second && second!=third && third==i+1){
+        count++;
+        p[second-1]=undefined;
+        p[first-1]=undefined;
+        p[i]=undefined;
+      }
     }
-  }
-  return(count/3);
+    return count;
 };
